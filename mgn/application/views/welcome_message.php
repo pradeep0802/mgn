d47e1ee74fd7c0a247da1dd855d766aa18759917
Pradeep Kumar <pradeep0802@gmail.com>
2015-05-28 13:04:05 +0530

    Pushing Application First Time to github

---
+++ mgn/application/views/welcome_message.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<title>Medical Globalnet</title>
+
+</head>
+<body>
+
+<div id="container">
+	<h1>Welcome!</h1>
+
+	<div id="body">
+		<p>This is default page.</p>
+
+		<p>The website is under construction. Please check us out after some days.</p>
+		
+	</div>
+
+	<p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds</p>
+</div>
+
+</body>
+</html>
